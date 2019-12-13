dee2153e31831710f2d244c5d98d3aa3d59285ef
Hugoos <hugo.spee@upcmail.nl>
2019-12-13 16:21:58 +0100

    Add files via upload

--- 2019/Day13-2-createVideo.py
+++ 2019/Day13-2-createVideo.py
@@ -6,12 +6,14 @@ import os
 img_array = []
 for filename in sorted(glob.glob('C:/Source/husp/Misc/2019/Day13Images/*.png'), key=os.path.getmtime):
     img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width,height)
-    img_array.append(img)
+    #height, width, layers = img.shape
+    dim = (420,210)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    img_array.append(resized)
  
- 
-out = cv2.VideoWriter('breakoutIntmachine.avi',cv2.VideoWriter_fourcc(*"XVID"), 30, size)
+#print(size)
+#out = cv2.VideoWriter('breakoutIntmachine.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, dim)
+out = cv2.VideoWriter('breakoutIntmachine.avi',cv2.VideoWriter_fourcc(*"xvid"), 30, dim)
  
 for i in range(len(img_array)):
     out.write(img_array[i])
