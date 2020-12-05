95580f0ea7fdb2973e756ff6604c99cb8747dab3
Hugo Spee <hugoos.spee@gmail.com>
2020-12-05 15:47:46 +0100

    AOC day 5 2020

---
+++ 2020/Day5-2.py
@@ -0,0 +1,29 @@
+import math
+import ast 
+from AOCTools import *
+fileName = "input/input5.txt" #pentest
+
+lines= read_strings(fileName)
+def binaryTraversal(size,instructions):
+    L = 0
+    R = size-1
+    for ins in instructions:
+        if ins == "F" or ins == "L":
+            R = R - ((R-L + 1) // 2)
+        elif ins == "B" or ins == "R":
+            L = L + ((R-L + 1) // 2)
+    if L == R:
+        return L
+    else:
+        print("error")
+
+idList = []
+for line in lines:
+    idList.append(binaryTraversal(128,line[:7]) * 8 + binaryTraversal(8,line[7:]))
+idList.sort(reverse=True)
+allIdList = [x for x in range(96,987)]
+notExistIdList = set(allIdList) -set(idList)
+#print(idList[0])
+#print(idList)
+print(notExistIdList)
+
