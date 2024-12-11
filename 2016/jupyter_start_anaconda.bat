d0a84e4243308d8caa7e764567ddecd13ea4e713
Hugoos <hugoos.spee@gmail.com>
2024-12-11 21:36:59 +0100

    day 11 2024

--- 2016/jupyter_start_anaconda.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-CALL "C:\Users\husp\Anaconda3\Scripts\activate.bat"
-CALL activate routing
-pushd %cd%
-call jupyter notebook
-pause
--- 2024/jupyter_start_anaconda.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-CALL "C:\Users\hugoo\anaconda3\Scripts\activate.bat"
-CALL activate aoc
-pushd %cd%
-call jupyter notebook
-pause
